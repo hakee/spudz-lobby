98b85f6017567fcf62575dab75dd8e8adb2123ed
RaulTsc <tomescu.raul@gmail.com>
2015-11-07 04:11:41 +0200

    added database save; started the REST API

--- config/initializers/facebookPassport.js
+++ config/initializers/facebookPassport.js
@@ -1,4 +1,4 @@
-module.exports = function(passport) {
+module.exports = function(passport, request) {
   passport.use(new FacebookStrategy({
       clientID: config.facebookLogin.clientID,
       clientSecret: config.facebookLogin.clientSecret,
@@ -6,15 +6,27 @@ module.exports = function(passport) {
     },
     function(accessToken, refreshToken, profile, done) {
       process.nextTick(function() {
-        console.log(accessToken, refreshToken, profile);
         return done(null, profile);
       });
     }
   ));
+
   passport.serializeUser(function(user, done) {
-    done(null, user);
+    request('http://localhost:8000/api/player/'+user.id, function(err, res, body) {
+      // Create it only if it doesn't already exist
+      if(!body) {
+        request.post('http://localhost:8000/api/player',
+                    {json: {uid: user.id,
+                            firstName: user.displayName.split(' ')[0],
+                            lastName: user.displayName.split(' ')[1] }});
+        done(null, user.id);
+      } else {
+        done(null, JSON.parse(body).uid);
+      }
+    });
   });
-  passport.deserializeUser(function(user, done) {
-    done(null, user);
+
+  passport.deserializeUser(function(id, done) {
+    done(null, id);
   });
 }
