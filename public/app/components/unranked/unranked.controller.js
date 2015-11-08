9da1d56235613f551de9d1e9f6e5eca395d6ef2e
Razvan Pat <razvan.pat@gmail.com>
2015-11-08 15:51:45 +0200

    added matchmaking screens

--- public/app/components/unranked/unranked.controller.js
+++ public/app/components/unranked/unranked.controller.js
@@ -5,14 +5,42 @@
 		Comms.addStateChangeListener(function(newState) {
 			console.log('currentState', newState);
 			$scope.currentState = newState;
+
+			$scope.showFindMatch = false;
+			$scope.showSearching = false;
+			$scope.showReady = false;
+			$scope.showClient = false;
+			$scope.showWaitingOp = false;
+
+			switch(newState) {
+				case 'beginReadyProcess':
+					$scope.showReady = true;
+					break;
+
+				case 'beginGame':
+					$scope.showClient = true;
+					break;
+
+			}
+
 			$scope.$digest();
 		});
 
+		$scope.showFindMatch = true;
+		$scope.showSearching = false;
+		$scope.showReady = false;
+		$scope.showClient = false;
+		$scope.showWaitingOp = false;
+
 		$scope.findMatch = function(){
+			$scope.showFindMatch = false;
+			$scope.showSearching = true;
 			Comms.findMatch();
 		}
 
 		$scope.readyUp = function(){
+			$scope.showReady = false;
+			$scope.showWaitingOp = true;
 			Comms.ready();
 		}
 	};
