f4bee3385d9292e8003f1b7ab63bd77d38db9325
Boogie <bbugarschi@gmail.com>
2015-11-08 13:10:13 +0200

    done Comms

--- public/app/shared/ws/ws.factory.js
+++ public/app/shared/ws/ws.factory.js
@@ -1,16 +1,56 @@
 function Comms($websocket){
-    
+    this.$socket = $websocket.$new();
+    this.handlers = [];
+    this.reset();
+
+    this.$socket.$on('$message', function(param){
+        if(param === 'end_match' && this.gameStarted){
+            this.reset();
+            this.handlers.forEach(function(h){
+                h("endGame");
+            }.bind(this));
+            return;
+        }
+        if(this.gameStarted){
+            return;
+        }
+        this.matchFound = this.matchFound || param === 'match_found';
+        this.gameStarted = this.gameStarted || param === 'start_character_selection';
+        this.handlers.forEach(function(h){
+            if(param === 'match_found' && !this.beginReady){
+                this.beginReady = true;
+                h("beginReadyProcess");
+                return;
+            }
+            if(param === 'start_character_selection'){
+                h('beginGame');
+                return ;
+            }
+        }.bind(this));
+    }.bind(this));
 }
 Comms.prototype.addStateChangeListener = function(handler){
-
+    this.handlers.push(handler);
 };
 Comms.prototype.ready = function(){
-
+    if(!this.gameStarted && this.matchFound && !this.readyUp){
+        this.readyUp = true;
+        this.$socket.$emit('player_ready');
+    }
 };
 
 Comms.prototype.findMatch = function(){
-
+    if(!this.gameStarted && !this.findingMatch){
+        this.findingMatch = true;
+        this.$socket.$emit('find_match');
+    }
 };
 
-
-module.exports = Comms
+Comms.prototype.reset = function(){
+    this.gameStarted = false;
+    this.matchFound = false;
+    this.findingMatch = false;
+    this.beginReady = false;
+    this.readyUp = false;
+};
+module.exports = Comms;
